6f1dad5d27588981c0ed1c3c6b69df0548018d8b
awais512 <raza.awais25@gmail.com>
2025-02-13 12:59:25 +0000

    Clerk Authentication added

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
-import React from "react";
 
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="rounded-full text-blue-600 hover:text-blue-500 border-blue-500/20 px-4 py-2 text-sm font-medium shadow-none"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="rounded-full text-blue-600 hover:text-blue-500 border-blue-500/20 px-4 py-2 text-sm font-medium shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
