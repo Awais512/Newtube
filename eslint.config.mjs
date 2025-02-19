0078aa636f6a206d4b8b54116233908a1942698f
awais512 <raza.awais25@gmail.com>
2025-02-19 22:50:12 +0000

    Eslint back

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       // Note: you must disable the base rule as it can report incorrect errors
-      "no-unused-vars": "off",
+      "no-unused-vars": "on",
       "@typescript-eslint/no-unused-vars": "error",
     },
   },
