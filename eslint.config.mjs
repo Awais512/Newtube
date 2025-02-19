b9f530d143e67475a4d6154a4cc0c41414046fc6
awais512 <raza.awais25@gmail.com>
2025-02-19 22:21:27 +0000

    Fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       // Note: you must disable the base rule as it can report incorrect errors
-      "no-unused-vars": "on",
+      "no-unused-vars": "of",
       "@typescript-eslint/no-unused-vars": "error",
     },
   },
