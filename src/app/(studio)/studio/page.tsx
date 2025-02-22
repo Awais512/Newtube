50c8f393dbe9d2e37b86cea820ffb8475c9ede7a
awais512 <raza.awais25@gmail.com>
2025-02-22 23:03:06 +0000

    Create video functionality

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,16 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 import React from "react";
 
-const Studio = () => {
-  return <div>Studio</div>;
+const Studio = async () => {
+  void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Studio;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/toaster";
 
 const font = Inter({
   subsets: ["latin"],
@@ -22,7 +23,10 @@ export default function RootLayout({
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
         <body className={`${font.className}`}>
-          <TRPCProvider>{children}</TRPCProvider>
+          <TRPCProvider>
+            <Toaster />
+            {children}
+          </TRPCProvider>
         </body>
       </html>
     </ClerkProvider>
